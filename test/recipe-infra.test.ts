b82607668d2cf58b9612f44f9aa3a92353d9bf7e
Philip Cali <philip.cali@gmail.com>
2022-11-11 22:25:21 -0500

    Initial commit

---
+++ test/recipe-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RecipeInfra from '../lib/recipe-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/recipe-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RecipeInfra.RecipeInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
